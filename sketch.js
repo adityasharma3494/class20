4f24e74360b20fe05dcb8daf7ebb2ba4bb3b30a1
adityasharma3494 <77100852+adityasharma3494@users.noreply.github.com>
2021-02-01 22:25:19 -1200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  movingRect = createSprite(100, 100, 80, 30);
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "red";
+
+}
+
+function draw() {
+  background(0);  
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  console.log(movingRect.x - fixedRect.x);
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2)  {
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+  } else {
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+  drawSprites();
+}
